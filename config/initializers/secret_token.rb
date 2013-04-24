0da860c5041915b498283b561deb3f3e5f8310c5
LISANDRO <lmcorbalan@gmail.com>
2013-04-24 18:40:07 -0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SILBApp::Application.config.secret_token = '5311e2aeaba9ab8e6ffc64c9a41dc76d2953ff4204736dea73c606c5884e5c9fdb14fe4c469e9450717f919584b7dd3a43422781c30258aaac299511ed86b35c'
